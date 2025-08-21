5dcd9651c7b248ea24566a8e63f9f3bbc853b2f3
BautistaPessagno <bpessagno@itba.edu.ar>
2025-08-21 08:45:29 -0300

    reorganization structs

--- src/include/game.h
+++ src/include/game.h
@@ -1,10 +1,28 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "player.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
 
 #define SHM_STATE "/game_state"
 #define SHM_SEM "/game_sync"
+
+typedef struct {
+    char playerName[16];      // Nombre del jugador
+    unsigned int score;       // Puntaje
+    unsigned int invalidMove; // Cantidad de solicitudes de movimientos inválidas realizadas
+    unsigned int validMove; // Cantidad de solicitudes de movimientos válidas realizadas
+    unsigned short qx, qy; // Coordenadas x e y en el tablero
+    pid_t pid;             // Identificador de proceso
+    char blocked;          // Indica si el jugador está bloqueado
+} player;
 
 typedef struct {
   unsigned short width;     // Ancho del tablero
